2f864bcbacda954d35331350aea59d61f8d10a2b
Paarth Gulati <p5gulati@uwaterloo.ca>
2025-12-14 20:10:44 -0500

    Added health checks for 3 services in Docker

--- app/main.py
+++ app/main.py
@@ -1,7 +1,52 @@
 from fastapi import FastAPI
+import psycopg2
+import chromadb
+
+db_params = {
+    "host": "postgres",
+    "database": "onboard_db",
+    "user": "user",
+    "password": "password",
+    "port": 5432
+}
 
 app = FastAPI()
 
 @app.get("/")
 async def test_endpoint():
     return {"message" : "Hello from inside Docker."}
+
+@app.get("/health")
+async def health_check():
+
+    postgres_status = "disconnected"
+    chromadb_status = "disconnected"
+
+    connection = None
+    try:
+        connection = psycopg2.connect(**db_params)
+        cursor = connection.cursor()
+        cursor.execute("SELECT 1")
+        cursor.fetchone()
+        postgres_status = "connected"
+
+    except Exception as error:
+        postgres_status = f"error: {str(error)}"
+    
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+    try:
+        chroma_client = chromadb.HttpClient(host='chromadb', port=8000)
+        chroma_connection = chroma_client.heartbeat()
+        chromadb_status = "connected"
+    except Exception as error:
+        chroma_status = f"error: {str(error)}"
+
+    return {
+        "fastapi": "ok",
+        "postgres": postgres_status,
+        "chromadb": chromadb_status
+    }
