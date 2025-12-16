9bc42e26212dd2a96fac8b54788a22d46c35fcdc
Paarth Gulati <p5gulati@uwaterloo.ca>
2025-12-15 21:41:36 -0500

    Added pydantic model for postgres data write

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import psycopg2
 import chromadb
+from pydantic import BaseModel
 
 db_params = {
     "host": "postgres",
@@ -50,3 +51,36 @@ async def health_check():
         "postgres": postgres_status,
         "chromadb": chromadb_status
     }
+
+class TextInput(BaseModel):
+    text: str
+
+@app.post("/test-pipeline")
+async def insert_data(data: TextInput):
+
+    text = data.text
+
+    try:
+        connection = psycopg2.connect(**db_params)
+        cursor = connection.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS test_data (
+                id SERIAL PRIMARY KEY,
+                text_content TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                chromadb_id VARCHAR(255))
+        """)
+        cursor.execute("INSERT INTO test_data (text_content) VALUES (%s) RETURNING id", (text,))
+        inserted_id = cursor.fetchone()[0]
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return {
+            "status": "success",
+            "message": "Data inserted",
+            "id": inserted_id,
+            "text": text
+        }
+    
+    except Exception as error:
+        return ({"status": "error", "message": str(error)})
